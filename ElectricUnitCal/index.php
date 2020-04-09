b2fa6f1cb110cdf62c5a81ee5aac0b95696121e0
ShivaniShri <63354003+ShivaniShri@users.noreply.github.com>
2020-04-09 14:18:59 +0530

    Section 1 of Home Page is Done.

---
+++ ElectricUnitCal/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Electricity Bill Calculator</title>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+<div class="d1">
+    <div class="d2">
+        <h1>Electricity Unit Consumed - Bill Calculator</h1>
+    </div>
+
+    <div class="d3">
+        <div class="d4">
+            <h2>Enter the Unit for Billing</h2>
+            <form method="post" action="index.php">
+                <input type="number" name="unit" placeholder="Enter the Electric Unit Consumed" required><br>
+                <input type="submit" name="submit" value="Calculate" required>
+            </form>
+        </div>
+    </div>
+
+</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script async defer src="script.js"></script>
+</body>
+</html>
